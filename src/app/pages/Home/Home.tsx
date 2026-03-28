ac1524f5a2bf269d00729e084e46e04d76c5e097
desarrollocb <desarrollo01@cbinformatica.net>
2026-03-27 19:00:04 -0600

    Violeta
    Se agregan rutas en el menú de Catálogos

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -65,67 +65,129 @@ const Home: React.FC = () => {
     },
   ];
 
-  const organizacionItems = [
-    { id: 'sucursales', title: 'Sucursales', path: routes.cat_Sucursales },
-    { id: 'areas', title: 'Áreas', path: routes.cat_Areas },
+  const comercialesItems = [
+    //Proveedores
+    { id: 'proveedores', title: 'Proveedores', path: routes.cat_Proveedores },
+    //Productos
+    { id: 'productos', title: 'Productos', path: routes.cat_Productos },
+    //Areas Departamentos y Clases
+    { id: 'areas', title: 'Áreas, Deptos y Clases', path: routes.cat_AreasDeptosClases },
+    //Catalogo de Marcas
     { id: 'marcas', title: 'Marcas', path: routes.cat_Marcas },
+    //Marcas-Familias
     { id: 'marcasFamilias', title: 'Marcas Familias', path: routes.Cat_Marcas_Familias },
+    //Clientes
+    { id: 'clientes', title: 'Clientes', path: routes.cat_Clientes },
+    //Alta de Plasticos Autorizados
+    { id: 'plasticos', title: 'Plásticos Autorizados', path: routes.cat_Plasticos_Autorizados },
+    //Administracion de Puntos
+    { id: 'porcentajesPuntos', title: 'Porcentajes y Puntos', path: routes.modulo_porcentajes_puntos },
+    //Tipos de descuento
+    { id: 'tipoDescuento', title: 'Tipo Descuentos', path: routes.cat_Tipo_Descuento },
+    //Administracion Insumos Servicios
+    { id: 'serviciosInsumos', title: 'Servicios e Insumos', path: routes.modulo_servicios_insumos },
+    //Configuracion de factores
+    { id: 'factores', title: 'Factores Sucursales', path: routes.factores_sucursal },
+    //Promociones Descto %
+    { id: 'promociones', title: 'Promociones', path: routes.config_promociones_descuento_porcentual },
+    //Descuento Proveedores
+    { id: 'descProveedores', title: 'Desc. Proveedores', path: routes.cat_DescProveedores },
   ];
 
-  const productosItems = [
-    { id: 'productos', title: 'Productos', path: routes.cat_Productos },
-    { id: 'clientes', title: 'Clientes', path: routes.cat_Clientes },
-    { id: 'proveedores', title: 'Proveedores', path: routes.cat_Proveedores },
+  const administracionyfinanzasItems = [
+    //Sucursales
+    { id: 'sucursales', title: 'Sucursales', path: routes.cat_Sucursales },
+    //Compradores
     { id: 'compradores', title: 'Compradores', path: routes.cat_Compradores },
-    { id: 'tipoDescuento', title: 'Tipo Descuentos', path: routes.cat_Tipo_Descuento },
-    { id: 'promociones', title: 'Promociones', path: routes.config_promociones_descuento_porcentual },
+    //Medios de pago
+    { id: 'mediosPago', title: 'Medios de Pago', path: routes.cat_Medios_Pago },
+    //Categorias de gastos
+    { id: 'categoriasGastos', title: 'Categorías de Gastos', path: routes.cat_Categorias },
+    //Proveedores Admon
+    { id: 'provAcreedores', title: 'Prov. Acreedores', path: routes.cat_Proveedores_Acreedores },
+    
+    
     { id: 'ajustePrecios', title: 'Ajuste Masivo Precios', path: '/ajustemasivoprecios' },
   ];
 
-  const serviciosItems = [
-    { id: 'serviciosInsumos', title: 'Servicios e Insumos', path: routes.modulo_servicios_insumos },
-    { id: 'porcentajesPuntos', title: 'Porcentajes y Puntos', path: routes.modulo_porcentajes_puntos },
+  const RRHHItems = [
+    //Lista de trabajadores y //Ficha de empleados
+    { id: 'trabajadores', title: 'Trabajadores', path: routes.cat_Trabajadores },
+    //Departamentos
+    { id: 'nominaDeptos', title: 'Deptos. Nómina', path: routes.cat_nomina_departamentos },
+    //Status
+    { id: 'nominaStatus', title: 'Status Nómina', path: routes.cat_nomina_Status },
+    //Bajas
+    { id: 'nominaBajas', title: 'Bajas Nómina', path: routes.cat_nomina_bajas },
+    //Formas de Pagos
+    { id: 'nominaFormasPago', title: 'Formas de Pago', path: routes.cat_nomina_formas_pagos },
+    //Puestos
+    { id: 'nominaPuestos', title: 'Puestos Nómina', path: routes.cat_nomina_puestos },
+    //Horarios
+    { id: 'nominasHorarios', title: 'Horarios', path: routes.cat_nominas_horarios },
+    //Niveles escolares
+    { id: 'nivelesEstudios', title: 'Niveles Estudios', path: routes.cat_niveles_estudios_rrhh },
+    //Dias festivos
+    { id: 'diasFestivos', title: 'Días Festivos', path: routes.cat_dias_festivos },
+    //Movimientos
+    { id: 'movimientos', title: 'Movimientos', path: routes.cat_nominas_movimientos },
+    //Tipos de Movimientos
+    { id: 'tiposMovimientos', title: 'Tipos Movimientos', path: routes.cat_tipos_movimientos },
+    //Conceptos de Ajustes
+    { id: 'conceptosAjustes', title: 'Conceptos Ajustes', path: routes.cat_conceptos_ajustes },
+    //Accesos alternos
+    { id: 'accesosAlternos', title: 'Accesos Alternos', path: routes.cat_accesosalternos },
+    //Comisiones
+    { id: 'comisiones', title: 'Comisiones', path: routes.cat_configcomisiones },
+    //Comisiones 2
+    { id: 'comisiones2', title: 'Comisiones 2', path: routes.cat_configcomisiones2 },
+    //Meta Sucursal
+    { id: 'metaSucursal', title: 'Meta Sucursal', path: routes.cat_metasucursal },
+    //Meta Empleados
+    { id: 'metaEmpleados', title: 'Meta Empleados', path: routes.cat_metaempleados },
   ];
 
-  const rrhhItems = [
-    { id: 'trabajadores', title: 'Trabajadores', path: routes.cat_Trabajadores },
-    { id: 'nominaDeptos', title: 'Deptos. Nómina', path: routes.cat_nomina_departamentos },
-    { id: 'nominaPuestos', title: 'Puestos Nómina', path: routes.cat_nomina_puestos },
-    { id: 'nominaStatus', title: 'Status Nómina', path: routes.cat_nomina_Status },
-    { id: 'nominaFormasPago', title: 'Formas de Pago', path: routes.cat_nomina_formas_pagos },
-    { id: 'nominaBajas', title: 'Bajas Nómina', path: routes.cat_nomina_bajas },
-    { id: 'asignacionHorarios', title: 'Asignación Horarios', path: routes.AsignacionHorarios },
-    { id: 'nominasHorarios', title: 'Horarios', path: routes.cat_nominas_horarios },
-    { id: 'diasFestivos', title: 'Días Festivos', path: routes.cat_dias_festivos },
-    { id: 'folios', title: 'Folios Nómina', path: routes.cat_nominas_folios },
-    { id: 'movimientos', title: 'Movimientos', path: routes.cat_nominas_movimientos },
-    { id: 'nivelesEstudios', title: 'Niveles Estudios', path: routes.cat_niveles_estudios_rrhh },
-    { id: 'turnosDobles', title: 'Turnos Dobles', path: routes.cat_turnos_dobles },
+
+   const contabilidadItems = [
+    //Catalogos de Tipos de Cuenta
+    { id: 'tiposCuentas', title: 'Tipos Cuentas', path: routes.cat_tipos_cuentas },
+    //Cuentas Bancarias
+    { id: 'cuentasBancarias', title: 'Cuentas Bancarias', path: routes.cat_cuentas_bancarias },
+  ];
+
+  const seguridadyaccesoItems = [
+    //Usuarios del sistema
+    { id: 'usuarios', title: 'Usuarios', path: routes.cat_usuarios },
+    //Perfiles y permisos
+    { id:'perfilesPermisos', title: 'Perfiles y Permisos', path: routes.cat_perfiles_permisos },
+    //Permisos por departamento comercial
+    { id:'permisosDeptos', title: 'Permisos por Deptos', path: routes.cat_PermisosDeptos },
   ];
 
   const comercialItems = [
     { id: 'retiros', title: 'Retiros', path: routes.retiros },
     { id: 'corteParcial', title: 'Corte Parcial', path: routes.corte_parcial },
     { id: 'corteDia', title: 'Corte Día', path: routes.corte_dia },
-    { id: 'plasticos', title: 'Plásticos Autorizados', path: routes.cat_Plasticos_Autorizados },
-    { id: 'mediosPago', title: 'Medios de Pago', path: routes.cat_Medios_Pago },
-    { id: 'factores', title: 'Factores Sucursales', path: routes.factores_sucursal },
+    
+    
+  ];
+  const rrhhItems = [
+    
+    
+    
+    
+    
+    
+    { id: 'asignacionHorarios', title: 'Asignación Horarios', path: routes.AsignacionHorarios },
+    
+    
+    { id: 'folios', title: 'Folios Nómina', path: routes.cat_nominas_folios },
+    
+    
+    { id: 'turnosDobles', title: 'Turnos Dobles', path: routes.cat_turnos_dobles },
   ];
 
-  const nominaConfigItems = [
-    { id: 'metaEmpleados', title: 'Meta Empleados', path: routes.cat_metaempleados },
-    { id: 'metaSucursal', title: 'Meta Sucursal', path: routes.cat_metasucursal },
-    { id: 'comisiones', title: 'Comisiones', path: routes.cat_configcomisiones },
-    { id: 'comisiones2', title: 'Comisiones 2', path: routes.cat_configcomisiones2 },
-    { id: 'cuentasBancarias', title: 'Cuentas Bancarias', path: routes.cat_cuentas_bancarias },
-    { id: 'tiposCuentas', title: 'Tipos Cuentas', path: routes.cat_tipos_cuentas },
-    { id: 'usuarios', title: 'Usuarios', path: routes.cat_usuarios },
-    { id: 'accesosAlternos', title: 'Accesos Alternos', path: routes.cat_accesosalternos },
-    { id: 'conceptosAjustes', title: 'Conceptos Ajustes', path: routes.cat_conceptos_ajustes },
-    { id: 'tiposMovimientos', title: 'Tipos Movimientos', path: routes.cat_tipos_movimientos },
-    { id: 'descProveedores', title: 'Desc. Proveedores', path: routes.cat_DescProveedores },
-    { id: 'provAcreedores', title: 'Prov. Acreedores', path: routes.cat_Proveedores_Acreedores },
-  ];
+ 
 
   return (
     <>
@@ -169,10 +231,10 @@ const Home: React.FC = () => {
               <div className={styles.catalogSubsection}>
                 <h3 className={styles.subsectionTitle}>
                   <GridView className={styles.sectionIcon} />
-                  Organización
+                  Comerciales
                 </h3>
                 <div className={styles.catalogsGrid}>
-                  {organizacionItems.map((catalog) => (
+                  {comercialesItems.map((catalog) => (
                     <button
                       key={catalog.id}
                       className={styles.catalogCard}
@@ -188,10 +250,10 @@ const Home: React.FC = () => {
               <div className={styles.catalogSubsection}>
                 <h3 className={styles.subsectionTitle}>
                   <Inventory className={styles.sectionIcon} />
-                  Productos
+                  Administración y finanzas
                 </h3>
                 <div className={styles.catalogsGrid}>
-                  {productosItems.map((catalog) => (
+                  {administracionyfinanzasItems.map((catalog) => (
                     <button
                       key={catalog.id}
                       className={styles.catalogCard}
@@ -207,10 +269,10 @@ const Home: React.FC = () => {
               <div className={styles.catalogSubsection}>
                 <h3 className={styles.subsectionTitle}>
                   <Settings className={styles.sectionIcon} />
-                  Servicios
+                  RRHH
                 </h3>
                 <div className={styles.catalogsGrid}>
-                  {serviciosItems.map((catalog) => (
+                  {RRHHItems.map((catalog) => (
                     <button
                       key={catalog.id}
                       className={styles.catalogCard}
@@ -226,10 +288,29 @@ const Home: React.FC = () => {
               <div className={styles.catalogSubsection}>
                 <h3 className={styles.subsectionTitle}>
                   <Assessment className={styles.sectionIcon} />
-                  Config. Nómina
+                  Contabilidad
                 </h3>
                 <div className={styles.catalogsGrid}>
-                  {nominaConfigItems.map((catalog) => (
+                  {contabilidadItems.map((catalog) => (
+                    <button
+                      key={catalog.id}
+                      className={styles.catalogCard}
+                      onClick={() => navigate(catalog.path)}
+                    >
+                      <Folder className={styles.catalogIcon} />
+                      <span>{catalog.title}</span>
+                    </button>
+                  ))}
+                </div>
+              </div>
+
+              <div className={styles.catalogSubsection}>
+                <h3 className={styles.subsectionTitle}>
+                  <Assessment className={styles.sectionIcon} />
+                  Seguridad y Acceso
+                </h3>
+                <div className={styles.catalogsGrid}>
+                  {seguridadyaccesoItems.map((catalog) => (
                     <button
                       key={catalog.id}
                       className={styles.catalogCard}
